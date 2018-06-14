c5cb4e81f86ecd71e4f7c7e98a80dd629ae536e0
Nicolas Vignes <nvigne@microsoft.com>
2018-06-14 20:42:33 +0200

    Project initialization

---
+++ NetHelper/EnumerableExtensions.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace NetHelper
+{
+    public static class EnumerableExtensions
+    {
+        public static bool IsNullOrEmpty<T>(this IEnumerable<T> enumerable)
+        {
+            return enumerable == null || !enumerable.Any();
+        }
+    }
+}
