7bdae7be99e2493366972c687c0af0d35d094674
Nicolas Vignes <nvigne@microsoft.com>
2018-06-15 21:24:46 +0200

    Adding tests for Enumerable Extensions

---
+++ NetHelperTests/EnumerableExtensionsTests.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using NetHelper;
+using Xunit;
+
+namespace NetHelperTests
+{
+    public class EnumerableExtensionsTests
+    {
+        [Fact]
+        public void IsNullOrEmptyReturnExpectedValue()
+        {
+            IEnumerable<string> enumerable = null;
+
+            Assert.True(enumerable.IsNullOrEmpty());
+
+            Assert.True(new List<string>().IsNullOrEmpty());
+
+            Assert.False((new List<string> { string.Empty}).IsNullOrEmpty());
+
+        }
+    }
+}
